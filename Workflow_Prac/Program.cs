794985827a2b15d3af88706aed398a7d130a0315
kaioc <kaioc@DAKOTA>
2025-03-18 12:45:59 +0200

    Add project files.

---
+++ Workflow_Prac/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Workflow_Prac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
